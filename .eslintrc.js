5a5c3e425ae42f4fd563975fb9d541c38b47d8ad
ananta <anantarajbastola@gmail.com>
2020-09-11 00:10:19 +0545

    :wrench: update eslint & ts config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,17 +7,20 @@ module.exports = {
             jsx: true, // Allows for the parsing of JSX
         },
     },
+    plugins: ['@typescript-eslint', 'import'],
     settings: {
         react: {
             version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
         },
         'import/resolver': {
+            typescript: {},
             'babel-module': {},
         },
     },
     extends: [
         'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
         'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
         'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
         'plugin:import/errors',
